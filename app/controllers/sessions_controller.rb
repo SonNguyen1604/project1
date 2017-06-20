ac4827cdba4622316a18278fbc4f381fe3876f68
SonNguyen1604 <sonnguyenngoc1604@gmail.com>
2017-06-20 14:35:17 +0700

    Finish advanced-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,10 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
+      alert = t ".login_success"
+      alert += user.name
+      flash[:success] = alert
+      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = t ".fail_login"
@@ -15,7 +19,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
+    flash[:success] = t ".logout"
     redirect_to root_url
   end
 end
