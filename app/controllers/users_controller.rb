bd6ff4d16a8021cfe8868701d00d188c707c828e
SonNguyen1604 <sonnguyenngoc1604@gmail.com>
2017-06-20 10:52:56 +0700

    Finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to @user
+      flash[:success] = t ".success"
+    else
+      flash.now[:danger] = t ".fail"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
